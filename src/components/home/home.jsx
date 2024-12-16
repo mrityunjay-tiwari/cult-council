344ec3eab3d1e45470f3003516d1bd821e47acb5
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2024-12-17 00:48:41 +0530

    updated

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,8 +3,8 @@ import WelcomePage from '../WelcomePage'
 import MedalShowcase from '../about/awardsShowcase'
 import Events from '../events/events'
 import Team from '../teams/team'
-import Carousel from '../clubs/imc/eventsImc'
-import RotatingMusicIntrument from '../rotatingMusicIntrument'
+// import Carousel from '../clubs/imc/eventsImc'
+// import RotatingMusicIntrument from '../rotatingMusicIntrument'
 
 const Home = () => {
   return (
