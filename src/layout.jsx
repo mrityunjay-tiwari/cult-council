a3037f7609255e3db7cba712c3f31a818312634b
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2024-12-17 00:54:52 +0530

    layout restored

---
+++ src/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationMenu from './components/header/NavBar'
+import Footer from './components/footer/footer'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+    <NavigationMenu />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout;
