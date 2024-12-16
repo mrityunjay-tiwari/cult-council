d16ce02cd5a0c7db464a841e9ac041b32a74f515
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2024-12-17 00:16:20 +0530

    flipwords removed

--- src/components/Flipwords.jsx
+++ src/components/Flipwords.jsx
@@ -1,48 +1,48 @@
-'use client'
-import React, { useCallback, useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import {cn} from './lib/utils';
+// 'use client'
+// import React, { useCallback, useEffect, useState } from "react";
+// import { AnimatePresence, motion } from "framer-motion";
+// import {cn} from './lib/utils';
 
-export const FlipWords = ({ words, duration = 1500, className }) => {
-  const [currentWord, setCurrentWord] = useState(words[0]);
-  const [isAnimating, setIsAnimating] = useState(false);
+// export const FlipWords = ({ words, duration = 1500, className }) => {
+//   const [currentWord, setCurrentWord] = useState(words[0]);
+//   const [isAnimating, setIsAnimating] = useState(false);
 
-  const startAnimation = useCallback(() => {
-    const word = words[words.indexOf(currentWord) + 1] || words[0];
-    setCurrentWord(word);
-    setIsAnimating(true);
-  }, [currentWord, words]);
+//   const startAnimation = useCallback(() => {
+//     const word = words[words.indexOf(currentWord) + 1] || words[0];
+//     setCurrentWord(word);
+//     setIsAnimating(true);
+//   }, [currentWord, words]);
 
-  useEffect(() => {
-    if (!isAnimating) setTimeout(() => {
-      startAnimation();
-    }, duration);
-  }, [isAnimating, duration, startAnimation]);
+//   useEffect(() => {
+//     if (!isAnimating) setTimeout(() => {
+//       startAnimation();
+//     }, duration);
+//   }, [isAnimating, duration, startAnimation]);
 
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={currentWord}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.5 }}
-        onAnimationComplete={() => { setIsAnimating(false); }}
-        className={cn("font-bold", className)}
-      >
-        {currentWord.split(" ").map((word, wordIndex) => (
-          <span key={`${word}-${wordIndex}`} className="flex">
-            {word.split("").map((letter, letterIndex) => (
-              <span key={`${letter}-${letterIndex}`} className="flex">
-                {letter}
-              </span>
-            ))}
-            {"\u00A0"}
-          </span>
-        ))}
-      </motion.div>
-    </AnimatePresence>
-  );
-};
+//   return (
+//     <AnimatePresence mode="wait">
+//       <motion.div
+//         key={currentWord}
+//         initial={{ opacity: 0, y: 20 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         exit={{ opacity: 0, y: -20 }}
+//         transition={{ duration: 0.5 }}
+//         onAnimationComplete={() => { setIsAnimating(false); }}
+//         className={cn("font-bold", className)}
+//       >
+//         {currentWord.split(" ").map((word, wordIndex) => (
+//           <span key={`${word}-${wordIndex}`} className="flex">
+//             {word.split("").map((letter, letterIndex) => (
+//               <span key={`${letter}-${letterIndex}`} className="flex">
+//                 {letter}
+//               </span>
+//             ))}
+//             {"\u00A0"}
+//           </span>
+//         ))}
+//       </motion.div>
+//     </AnimatePresence>
+//   );
+// };
 
-// export default FlipWords;
+// // export default FlipWords;
