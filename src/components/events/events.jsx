4b990224d0b0f4417e9a8c0c924999f1888ee155
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2024-12-18 00:13:51 +0530

    overflow update2

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -422,7 +422,7 @@ const ParallaxGallery = () => {
   ];
 
   return (
-    <div className="relative h-screen overflow-x-hidden bg-black w-screen">
+    <div className="relative h-screen overflow-hidden bg-black w-screen">
       <div className="absolute top-0 h-20 bg-gradient-to-b from-slate-900 to-transparent z-10 pointer-events-none" />
       
       <div className="absolute inset-0 flex justify-between gap-2 md:gap-4 px-2 md:px-4">
