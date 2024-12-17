fde80bd1a3ec7a0836d57b78e41906009d47ade0
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2024-12-18 00:17:31 +0530

    team name update

--- src/components/techTeam.jsx
+++ src/components/techTeam.jsx
@@ -489,7 +489,7 @@ const VideoTeamDisplay = () => {
       color: 'bg-blue-100',
     },
     {
-      name: 'Laxmi Kumari',
+      name: 'Nishant Kumar',
       role: 'Tech Manager',
       avatar: 'PP.jpg',
       color: 'bg-purple-100',
